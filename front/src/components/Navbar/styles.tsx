a5a78563056544dd72edb7be1ccfe4a9026b7471
= <f7lipe@me.com>
2022-12-11 23:15:03 -0300

    Update styles.tsx

--- front/src/components/Navbar/styles.tsx
+++ front/src/components/Navbar/styles.tsx
@@ -8,12 +8,14 @@ const Nav = styled.nav`
     align-items: center;
     padding: 0 2rem;
     color: #000000;
-    background-color: white;
     position: fixed;
     top: 0;
     left: 0;
     right: 0;
     z-index: 1;
+
+  -webkit-backdrop-filter: blur(20px);
+  backdrop-filter: blur(20px);
 `;
 
 const Beta = styled.span`
