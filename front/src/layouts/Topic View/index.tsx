73640f1475ceccd2d15fa7ffb9ba2f8e2ceca030
= <f7lipe@me.com>
2022-12-20 11:12:23 -0300

    implements topic toolbar

--- front/src/layouts/Topic View/index.tsx
+++ front/src/layouts/Topic View/index.tsx
@@ -3,24 +3,19 @@ import { HStack } from "../../components/Stack View/HStack"
 import { VStack } from "../../components/Stack View/VStack"
 import { LargeHeading, Subtitle } from "../../components/Typografies"
 import Reader from "../../components/Reader"
-import { EditButton, TableOfContents, Item } from "./styles"
 import { useEffect, useState, useLayoutEffect } from "react"
 import Navbar from "../../components/Navbar"
 import useTopic from "../../hooks/useTopic"
 import { useParams } from "react-router-dom"
-import { ProgressBar } from 'react-loader-spinner'
 import generateTOC from "../../helpers/generateTOC"
-
-import { IoInformationCircleOutline, IoTrashOutline, IoArchiveOutline } from "react-icons/io5"
+import Toolbar from "../../components/Toolbar"
+import TOC from "../../components/TOC"
 
 const TopicView = () => {
     const { id } = useParams<{id: string}>()
-    const { topic, getTopic, isEditing, setIsEditing, status, cleanUp } = useTopic()
+    const { topic, getTopic, isEditing, cleanUp } = useTopic()
     const { content } = topic 
     const [toc, setToc] = useState<String[] | []>([])
-    const handleEdit = () => {
-        setIsEditing(!isEditing)
-    }
     
     useEffect(() => {
         if (id) getTopic(id)
@@ -48,31 +43,7 @@ const TopicView = () => {
                 <VStack
                     align="flex-start">
                     <LargeHeading>{topic.title}</LargeHeading>
-                    <HStack
-                        width="150px"
-                        justify={isEditing ? "flex-start" : "space-between"}
-                        align="center"
-                        margin="-50px 0 0 0">
-                        <EditButton
-                            editing={isEditing}
-                            onClick={handleEdit}>
-                            {isEditing ? 
-                                (status === 'saving' ? <ProgressBar height="40"  width="80" ariaLabel="progress-bar-loading" borderColor = '#F4442E' barColor = '#51E5FF'/> : 'Done')
-                                : 'Edit'}
-                        </EditButton>
-                        <IoInformationCircleOutline
-                            size={"20px"} />
-                        {
-                            !isEditing && (
-                                <>
-                                    <IoArchiveOutline
-                                        size={"20px"} />
-                                    <IoTrashOutline
-                                        size={"20px"} />
-                                </>
-                            )
-                        }
-                    </HStack>
+                    <Toolbar />
                 </VStack>
             </HStack>
             <HStack
@@ -81,14 +52,7 @@ const TopicView = () => {
                 justify="flex-start"
                 margin="0 0 30px 0">
                 {
-                    (!isEditing && toc?.length > 0) && (
-                        <TableOfContents>
-                            <Subtitle>TABLE OF CONTENTS</Subtitle>
-                            {
-                                toc.map((item, index) => (<Item key={index}>{item}</Item>))
-                            }
-                        </TableOfContents>
-                    )
+                    (!isEditing && toc?.length > 0) && <TOC toc={toc} />  
                 }
             </HStack>
             {isEditing && id ? <Tiptap  topicId={id}/> : <Reader />}
--- front/src/layouts/Topic View/styles.tsx
+++ front/src/layouts/Topic View/styles.tsx
@@ -4,7 +4,7 @@ interface EditButtonProps {
     editing: boolean
 }
 
-const EditButton = styled.a`
+const EditButton = styled.button`
     font-size: medium;
     font-weight: ${(props: EditButtonProps) => props.editing ? 300 : 800};
     font-weight: 500;
@@ -12,28 +12,8 @@ const EditButton = styled.a`
     margin-right: 10px;
     margin-left: 10px;
     cursor: pointer;
+    border: none;
+    background: none;
+    outline: none;
 `
-
-const TableOfContents = styled.ol`
-    margin: 0;
-    padding: 0 0 10px 10px;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-    border: 2px solid #1f1f1f;
-    border-radius: 5px;
-`
-
-const Item = styled.li`
-    margin-left: 20px;  
-    margin-bottom: 10px;
-    padding: 0;
-    font-size: large;
-    font-weight: 500;
-    color: #1f1f1f;
-    cursor: pointer;
-`
-
-export {EditButton, TableOfContents, Item}
+export {EditButton}
