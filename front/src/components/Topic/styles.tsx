64fd267d288be3a07f037e8171be8bdb3316d821
= <f7lipe@me.com>
2022-12-20 11:11:36 -0300

    Update styles.tsx

--- front/src/components/Topic/styles.tsx
+++ front/src/components/Topic/styles.tsx
@@ -4,7 +4,7 @@ const Body = styled.div`
     width: 170px;
     height: 170px;
     margin:5px;
-    background-color: #e2e2e28c;
+    //background-color: #e2e2e28c;
     border-radius: 10px;
     display: flex;
     flex-direction: column;
@@ -14,7 +14,7 @@ const Body = styled.div`
     cursor: pointer;
     transition: all 0.2s ease-in-out;
     &:hover {
-        background-color: #cbfeaa;
+        background-color: #fffce4;
     }
 `
 
