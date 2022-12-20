2f38cede94eb5caa77ba711c0f056de66c89058b
= <f7lipe@me.com>
2022-12-20 14:07:29 -0300

    Update TopicContext.tsx

--- front/src/contexts/TopicContext.tsx
+++ front/src/contexts/TopicContext.tsx
@@ -1,11 +1,13 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useCallback } from 'react'
 import axios from "axios"
 
 
 interface ITopicContext {
+    id: string | undefined
+    setId: (id: string) => void
     createTopic: (title: string, content?: string) => Promise<void>
-    getTopics: () => Promise<void>
-    getTopic: (_id: string) => Promise<void>
+    getTopics: () => void
+    getTopic: () => void
     editTopic: (_id: string, title?: String, content?: string) => Promise<void>
     deleteTopic?: (_id: string) => Promise<void>
     archiveTopic?: (_id: string) => Promise<void>
@@ -28,6 +30,7 @@ const TopicContext = createContext<ITopicContext | null>(null)
 //const API = process.env.API
 
 const TopicProvider = ({ children } : ITopicProvider) => {
+    const [id, setId] = useState<string | undefined>()
     const [isEditing, setIsEditing] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState('')
@@ -37,34 +40,40 @@ const TopicProvider = ({ children } : ITopicProvider) => {
     const createTopic = async (title: string, content?: string) => {
     }
 
-    const getTopics = async () => {
-        try {
-            setIsLoading(true)
-            const response = await axios.get(`https://639a7d283a5fbccb5268037a.mockapi.io/topics`)
-            const data: Topic[] = await response.data
-            setTopics(data)
-            setIsLoading(false)
-        } catch (error: any) {
-            setIsLoading(false)
-            setError(error.message)
-            console.log("error: ", error)
+    const getTopics = useCallback(()=>{
+       const getAllTopics =  async () => {
+            try {
+                setIsLoading(true)
+                const response = await axios.get(`https://639a7d283a5fbccb5268037a.mockapi.io/topics`)
+                const data: Topic[] = await response.data
+                setTopics(data)
+                setIsLoading(false)
+            } catch (error: any) {
+                setIsLoading(false)
+                setError(error.message)
+                console.log("error: ", error)
+            }
         }
-    }
+        getAllTopics()
+    }, [])
 
-    const getTopic = async (_id: string) => {
-        try{
-            setIsLoading(true)
-            const response = await axios.get(`https://639a7d283a5fbccb5268037a.mockapi.io/topics/${_id}`)
-            const data: Topic = await response.data
-            setTopic(data)
-            setIsLoading(false)
-        } catch (error: any) {
-            setIsLoading(false)
-            setError(error.message)
-            console.log("error: ", error)
+    const getTopic = useCallback( ()=>{
+      const fetch =  async () => {
+            try{ 
+                setIsLoading(true)
+                const response = await axios.get(`https://639a7d283a5fbccb5268037a.mockapi.io/topics/${id}`)
+                const data: Topic = await response.data
+                setTopic(data)
+                setIsLoading(false)
+            } catch (error: any) {
+                setIsLoading(false)
+                setError(error.message)
+                console.log("error: ", error)
+            }
+        
         }
-    
-    }
+        fetch()
+    },[id])
 
     const editTopic = async (_id: string, title?: String, content?: string) => {
         try{
@@ -89,18 +98,22 @@ const TopicProvider = ({ children } : ITopicProvider) => {
     const archiveTopic = async (_id: string) => {
     }
 
-    const cleanUp = () => {
-        setIsEditing(false)
-        setIsLoading(false)
-        setError('')
-        setTopics([])
-        setTopic({} as Topic)
-        setStatus('idle')
-    }
+    const cleanUp = useCallback(() => {
+        // if(status === 'saved') {...}
+            setId(undefined)
+            setIsEditing(false)
+            setIsLoading(false)
+            setError('')
+            setTopics([])
+            setTopic({} as Topic)
+            setStatus('idle')
+    }, [])
 
     return (
         <TopicContext.Provider value={
-            { 
+            {   
+                id,
+                setId,
                 createTopic, 
                 getTopics,
                 getTopic, 
