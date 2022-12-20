226f6cfd504bf2396233ccc4c6df48e1d3751094
= <f7lipe@me.com>
2022-12-20 17:05:23 -0300

    feat: create topic

--- front/src/components/New Topic/index.tsx
+++ front/src/components/New Topic/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react"
 import { Background, Body, Button, NewTopicForm, Input } from "./styles"
 import { AiOutlineCloseCircle } from "react-icons/ai"
 import { HStack } from "../Stack View/HStack"
 import { SmallHeading } from "../Typografies"
+import useTopic from "../../hooks/useTopic"
 
 interface INewTopicProps {
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
@@ -9,21 +11,38 @@ interface INewTopicProps {
 
 const NewTopic = (props: INewTopicProps) => {
     const { setOpen } = props
+    const { createTopic } = useTopic()
+    const [title, setTitle] = useState('')
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        await createTopic(title)
+    }
+
     return (
         <Background>
             <Body>
                 <HStack
                     justify="flex-end"
                     height="20px">
-                    <AiOutlineCloseCircle size={25} onClick={()=> setOpen(false)}/>
+                    <AiOutlineCloseCircle
+                        size={25}
+                        onClick={() => setOpen(false)} />
                 </HStack>
-                <HStack height="30px" justify="flex-start">
+                <HStack
+                    height="30px"
+                    justify="flex-start">
                     <SmallHeading>New Topic</SmallHeading>
                 </HStack>
-                <NewTopicForm>
-                    <Input type="text" placeholder="Title" />
+                <NewTopicForm onSubmit={handleSubmit}>
+                    <Input
+                        type="text"
+                        placeholder="Title"
+                        onChange={(e) => setTitle(e.target.value)} />
+                    <Button
+                        type="submit">
+                        Create Topic
+                    </Button>
                 </NewTopicForm>
-                <Button> Create Topic </Button>
             </Body>
         </Background>
     )
