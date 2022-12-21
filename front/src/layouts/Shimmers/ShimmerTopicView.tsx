f95c151262339a45d54ee9837bc6c6551b6538c8
= <f7lipe@me.com>
2022-12-21 15:49:25 -0300

    Update ShimmerTopicView.tsx

--- front/src/layouts/Shimmers/ShimmerTopicView.tsx
+++ front/src/layouts/Shimmers/ShimmerTopicView.tsx
@@ -11,7 +11,7 @@ const ShimmerTopicView = () => {
         margin="50px 0 30px 0">
             <VStack align="flex-start">
                 <Shimmer width="70%" height="60px"  margin={"40px 0 0 0"} />
-                <Shimmer width="50%" height="25px" margin={"10px 0 20px 0"} />
+                <Shimmer width="15%" height="25px" margin={"10px 0 20px 0"} />
                 <Shimmer width="100%" height="300px" margin={"10px 0 0 0"} />
             </VStack>
         </HStack>
