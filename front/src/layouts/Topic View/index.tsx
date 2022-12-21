dd3f58a35795477dc8377a9794dd98faa5cfd0b5
= <f7lipe@me.com>
2022-12-21 15:45:33 -0300

    Update index.tsx

--- front/src/layouts/Topic View/index.tsx
+++ front/src/layouts/Topic View/index.tsx
@@ -9,15 +9,16 @@ import Toolbar from "../../components/Toolbar"
 import TOC from "../../components/TOC"
 import useTopic from "../../hooks/useTopic"
 import generateTOC from "../../helpers/generateTOC"
+import ShimmerTopicView from "../Shimmers/ShimmerTopicView"
 
 const TopicView = () => {
-    const { topic, getTopic, isEditing, cleanUp, id } = useTopic()
-    const { content } = topic 
+    const { topic, getTopic, isEditing, cleanUp, id, isLoading } = useTopic()
+    const { content } = topic
     const [toc, setToc] = useState<String[] | []>([])
 
     useEffect(() => {
-            getTopic()
-   
+        getTopic()
+
         return () => {
             cleanUp()
         }
@@ -33,28 +34,34 @@ const TopicView = () => {
         <VStack
             align="center"
             width="100%">
-                <Navbar />
-            <HStack
-                widthMobile="95%"
-                width="70%"
-                justify="flex-start"
-                margin="50px 0 30px 0">
-                <VStack
-                    align="flex-start">
-                    <LargeHeading>{topic.title}</LargeHeading>
-                    <Toolbar />
-                </VStack>
-            </HStack>
-            <HStack
-                widthMobile="95%"
-                width="70%"
-                justify="flex-start"
-                margin="0 0 30px 0">
-                {
-                    (!isEditing && toc?.length > 0) && <TOC toc={toc} />  
-                }
-            </HStack>
-            {isEditing && id ? <Tiptap  topicId={id}/> : <Reader />}
+            <Navbar />
+            {
+                isLoading ? <ShimmerTopicView /> : (
+                    <>
+                        <HStack
+                            widthMobile="95%"
+                            width="70%"
+                            justify="flex-start"
+                            margin="50px 0 30px 0">
+                            <VStack
+                                align="flex-start">
+                                <LargeHeading>{topic.title}</LargeHeading>
+                                <Toolbar />
+                            </VStack>
+                        </HStack>
+                        <HStack
+                            widthMobile="95%"
+                            width="70%"
+                            justify="flex-start"
+                            margin="0 0 30px 0">
+                            {
+                                (!isEditing && toc?.length > 0) && <TOC toc={toc} />
+                            }
+                        </HStack>
+                        {isEditing && id ? <Tiptap /> : <Reader />}
+                    </>
+                )
+            }
         </VStack>
     )
 }
