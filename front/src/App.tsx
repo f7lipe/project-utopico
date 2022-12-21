62bd5c4eeeaf9bbc613a372b26d002ff87364809
= <f7lipe@me.com>
2022-12-21 15:45:24 -0300

    Update App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,22 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Topics from "./layouts/Topics"
 import TopicView from "./layouts/Topic View"
-import { EditorProvider } from "./contexts/EditorContext"
 import { TopicProvider } from "./contexts/TopicContext"
 
 function App() {
   return (
-    <TopicProvider>
     <BrowserRouter>
       <Routes>
-        <Route path="/topics" element={<Topics />} />
+        <Route path="/topics" element={
+          <TopicProvider>
+            <Topics />
+          </TopicProvider>
+        } />
         <Route path="/topics/:id" element={
-          <EditorProvider>
+          <TopicProvider>
             <TopicView />
-          </EditorProvider>} />
+          </TopicProvider>
+        } />
       </Routes>
     </BrowserRouter>
-    </TopicProvider>
   );
 }
 
