e45b89a6ab5a9909c18fcb6498638d26265cb924
= <f7lipe@me.com>
2022-12-27 20:35:46 -0300

    Create ShimmerTopics.tsx

---
+++ front/src/layouts/Topics/Shimmer/ShimmerTopics.tsx
@@ -0,0 +1,17 @@
+import { HStack } from "../../../components/Stack View/HStack"
+import { VStack } from "../../../components/Stack View/VStack"
+import { Shimmer } from "../../Shimmer Effect"
+
+const ShimmerTopics = () => {
+    return (
+        <HStack>
+            <VStack align="flex-start">
+                <Shimmer width="100%" height="60px" borderRadius={"10px"} margin={"0 0 30px 0 "} />
+                <Shimmer width="300px" height="55px" margin={"10px 0 0 0"} />
+                <Shimmer width="140px" height="140px" margin={"10px 0 0 0"} />
+            </VStack>
+        </HStack>
+    )
+}
+
+export default ShimmerTopics
