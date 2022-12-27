026cffd47e5021057dc477acc4cf6c374a10f8c1
= <f7lipe@me.com>
2022-12-27 10:04:57 -0300

    Update styles.tsx

--- front/src/components/New Topic/styles.tsx
+++ front/src/components/New Topic/styles.tsx
@@ -1,36 +1,7 @@
 import styled from "styled-components"
-
-const Background = styled.div`
-    height: 100vh;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    position:fixed;
-    top: 80px;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    z-index: 1;
-    -webkit-backdrop-filter: blur(20px);
-     backdrop-filter: blur(20px);
-`
-
-const Body = styled.div`
-    width: 300px;
-    border: 2px solid #000;
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 20px;
-
-    @media (max-width: 768px) {
-        width: 90%;
-    }
-`
+interface Props {
+    visible: boolean
+}
 
 const Button = styled.button`
     width: 100%;
@@ -41,31 +12,29 @@ const Button = styled.button`
     color: #fff;
     font-size: 16px;
     font-weight: 600;
-    margin-top: 20px;
     cursor: pointer;
-    display: flex;
+    display: ${(props: Props) => props.visible ? "flex" : "none"};
     align-items: center;
     justify-content: center;
+    margin-top: 10px;
+    :disabled {
+        background-color: #ccc;
+        cursor: not-allowed;
+    }
 `
 
 const NewTopicForm = styled.form`
-    width: 100%;
+    width: 30%;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: flex-start;
     justify-content: center;
-`
-const Input = styled.input`
-    width: 100%;
-    height: 40px;
-    border: 1px solid #000;
-    font-weight: bolder;
-    font-size: 16px;
-    color: black;
-    background-color: #e6e1e147;
-    border-radius: 10px;
-    padding: 10px;
-    margin-top: 20px;
+    padding-bottom: 10px;
+    margin-top: 10px;
+    @media (max-width: 768px) {
+        width: 100%;
+    }
 `
 
-export { Background, Body, Button, NewTopicForm, Input } 
+
+export { Button, NewTopicForm } 
