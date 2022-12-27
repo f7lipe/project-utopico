482ec50476793aecd895dc2227aa36b329fcaa97
= <f7lipe@me.com>
2022-12-27 10:34:34 -0300

    Refact: Topic Toolbar

---
+++ front/src/layouts/Topics/Topic Toolbar/index.tsx
@@ -0,0 +1,12 @@
+import {TopicToolbar} from '../styles'
+import NewTopic from '../../../components/New Topic'
+
+const Toolbar = () => {
+    return (
+        <TopicToolbar>
+            <NewTopic />
+        </TopicToolbar>
+    )
+}
+
+export default Toolbar
