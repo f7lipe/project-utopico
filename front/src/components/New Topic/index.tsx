84878559b6ed8c1a120fac96aede399b1c5cd35c
= <f7lipe@me.com>
2022-12-27 10:05:02 -0300

    Update index.tsx

--- front/src/components/New Topic/index.tsx
+++ front/src/components/New Topic/index.tsx
@@ -1,59 +1,62 @@
-import { useState } from "react"
-import { Background, Body, Button, NewTopicForm, Input } from "./styles"
-import { AiOutlineCloseCircle } from "react-icons/ai"
-import { HStack } from "../Stack View/HStack"
-import { SmallHeading } from "../Typografies"
+import { Button, NewTopicForm } from "./styles"
+import { useFormik } from 'formik';
 import useTopic from "../../hooks/useTopic"
 import PropagateLoader from "react-spinners/PropagateLoader"
+import TextField from '@mui/material/TextField'
+import * as Yup from 'yup'
+import useNetwork from "../../hooks/useNetwork";
 
-interface INewTopicProps {
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-const NewTopic = (props: INewTopicProps) => {
-    const { setOpen } = props
-    const { createTopic, status } = useTopic()
-    const [title, setTitle] = useState('')
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        await createTopic(title)
-        setOpen(!(status === "idle"))
-    }
+const NewTopic = () => {
+    const { createTopic, status, error } = useTopic()
+    const {state} = useNetwork()
+    const offline = !state.online
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+        },
+        validationSchema: Yup.object({
+            title: Yup.string()
+                .max(30, 'Must be 30 characters or less')
+                .min(3, 'Must be 3 characters or more')
+                .required('Required')
+                .matches(/^(\S+$)/g, 'Must not start with a space'),
+        }),
+        onSubmit: async (values) => {
+            await createTopic(values.title)
+        }
+    })
 
     return (
-        <Background>
-            <Body>
-                <HStack
-                    justify="flex-end"
-                    height="20px">
-                    <AiOutlineCloseCircle
-                        size={25}
-                        onClick={() => setOpen(false)} />
-                </HStack>
-                <HStack
-                    height="30px"
-                    justify="flex-start">
-                    <SmallHeading>New Topic</SmallHeading>
-                </HStack>
-                <NewTopicForm onSubmit={handleSubmit}>
-                    <Input
-                        type="text"
-                        placeholder="Title"
-                        disabled = { status === "creating" }
-                        onChange={(e) => setTitle(e.target.value)} />
-                    <Button disabled = {status === "creating"}
-                        type="submit">
-                        { 
-                        status === "creating" ? 
-                            <PropagateLoader 
-                            color="#36d7b7" size={4} /> 
-                            : 
-                            "Create topic"
-                            }
-                    </Button>
-                </NewTopicForm>
-            </Body>
-        </Background>
+
+        <NewTopicForm onSubmit={formik.handleSubmit} >
+            <TextField
+                id="title"
+                name="title"
+                label="What's next?"
+                variant="standard"
+                type="text"
+                fullWidth
+                value={formik.values.title}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                disabled={status === "creating" || offline}
+                error={formik.touched.title && Boolean(formik.errors.title) || Boolean(error)}
+                helperText={formik.touched.title && formik.errors.title || error}
+            />
+            <Button
+                visible={formik.values.title.length > 2}
+                disabled={status === "creating" || !formik.isValid || offline}
+                type="submit">
+                {
+                    status === "creating" ?
+                        <PropagateLoader
+                            color="#36d7b7" size={4} />
+                        :
+                        "Create topic"
+                }
+            </Button>
+        </NewTopicForm>
+
     )
 }
 
