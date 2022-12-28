6215da1526a0cb6808ce0202c2140a6bfa198d0d
= <f7lipe@me.com>
2022-12-28 10:36:28 -0300

    Update styles.tsx

--- front/src/components/Collection View/styles.tsx
+++ front/src/components/Collection View/styles.tsx
@@ -5,6 +5,10 @@ const Container = styled.section`
     flex-wrap: wrap;
     align-items: center;
     justify-content: flex-start;
+    
+    @media (max-width: 768px) {
+        justify-content: center;
+    }
 `
 
 export default Container;
