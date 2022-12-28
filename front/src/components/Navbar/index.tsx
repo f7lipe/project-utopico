1cc3f1ec9e40374015b5aedde4ba9f26831d485d
= <f7lipe@me.com>
2022-12-28 10:05:30 -0300

    Refac: Navbar
    Includes MobileLogo apart to be fixed into the screen top in mobile devices.
    
    NavLinks are now styled with styled components instead.

--- front/src/components/Navbar/index.tsx
+++ front/src/components/Navbar/index.tsx
@@ -1,53 +1,66 @@
-import { Nav, Beta, NavLinks, NavLink } from "./styles"
-import { Link, useLocation } from "react-router-dom"
-import { HStack } from "../Stack View/HStack"
-import { AiOutlineGithub, /*AiOutlineSearch*/ } from "react-icons/ai"
+import { Nav, Logo, MobileLogo, Beta, NavLinks, NavLink } from "./styles"
+import { useLocation } from "react-router-dom"
+import { AiOutlineGithub, AiOutlineSearch } from "react-icons/ai"
 import { RxText } from "react-icons/rx"
 
 enum Paths {
     SEARCH = "/search",
-    GITHUB = "/github",
-    LOGOUT = "/",
+    GITHUB = "https://github.com/f7lipe/project-utopico",
+    PROFILE = "/profile",
     TOPICS = "/topics"
 }
 
 const Navbar = () => {
-    const location = useLocation();
-    /*
+    const location = useLocation()
+
     const isActive = (buttonPath: string) => {
         return location.pathname === buttonPath;
-      }
-      */
+    }
 
     return (
-        <Nav>
-            <HStack width="150px">
+        <>
+            <MobileLogo >
                 <p>utópico</p>
                 <Beta>1.0 BETA</Beta>
-            </HStack>
-            <NavLinks>
-                <NavLink active={(location.pathname.includes(Paths.TOPICS))}>
-                    <RxText size="20px" />
-                    <Link to={Paths.TOPICS}>Topics</Link>
-                </NavLink>
-                 {/* 
-                <NavLink active={isActive(Paths.SEARCH)}>
-                    <AiOutlineSearch size="20px" />
-                    <Link to={Paths.SEARCH}>Search</Link>
-                </NavLink>
-                 */}
-                <NavLink>
-                    <AiOutlineGithub size="20px" />
-                    <Link to={Paths.GITHUB}>GitHub</Link>
-                </NavLink>
-                {/* 
-                <NavLink>
-                    <Link to={Paths.LOGOUT}>Logout</Link>
-                </NavLink>
-                */}
-            </NavLinks>
-        </Nav>
-    );
-};
+            </MobileLogo>
+            <Nav>
+                <Logo >
+                    <p>utópico</p>
+                    <Beta>1.0 BETA</Beta>
+                </Logo>
 
-export default Navbar;
+                <NavLinks>
+
+                    <NavLink
+                        to={Paths.TOPICS}
+                        active={String(location.pathname.includes(Paths.TOPICS))}>
+                        <RxText size="20px" />
+                        Topics
+                    </NavLink>
+
+                    <NavLink
+                        to={Paths.SEARCH}
+                        active={String(isActive(Paths.SEARCH))}>
+                        <AiOutlineSearch size="20px" />
+                        Search
+                    </NavLink>
+
+                    <NavLink
+                        to={""}
+                        onClick={() => window.location.href = Paths.GITHUB}>
+                        <AiOutlineGithub size="20px" />
+                        GitHub
+                    </NavLink>
+
+                    {/* 
+                    <NavLink to={Paths.Profile}>
+                        Profile
+                    </NavLink>
+                    */}
+                </NavLinks>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
--- front/src/components/Navbar/styles.tsx
+++ front/src/components/Navbar/styles.tsx
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Nav = styled.nav`
     display: flex;
@@ -13,9 +14,17 @@ const Nav = styled.nav`
     left: 0;
     right: 0;
     z-index: 1;
+    backdrop-filter: blur(20px);
+    -webkit-backdrop-filter: blur(20px);
 
-  -webkit-backdrop-filter: blur(20px);
-  backdrop-filter: blur(20px);
+  
+  @media (max-width: 768px) {
+    top: auto;
+    bottom: 0;
+    border-top: 1px solid #4f4f4f;
+    background-color: white;
+    justify-content: center;
+    }
 `;
 
 const Beta = styled.span`
@@ -27,47 +36,83 @@ const Beta = styled.span`
     font-weight: bolder;
 `;
 
-const Logo = styled.img`
-    height: 3rem;
+const Logo = styled.div`
+    display: flex;
+    align-items: center;
+    width: 150px;
+    @media (max-width: 768px) {
+    display: none;
+    }
 `;
+
+const MobileLogo = styled.div`
+    display: flex;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 80px;
+    padding-left: 15px;
+    backdrop-filter: blur(20px);
+    -webkit-backdrop-filter: blur(20px);
+
+    @media (min-width: 768px) {
+    display: none;
+    }
+`
 
 const NavLinks = styled.ul`
     display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding-left: 0;
     list-style: none;
+
+    @media (max-width: 768px) {
+        width: 100%;
+    }
 `;
 
 interface NavLinkProps {
-    active?: boolean;
+    active?: string; // DOM elements can't have boolean attributes
 }
 
-const NavLink = styled.li`
-
+const NavLink = styled(Link)`
     height: 40px;
-    margin: 0 1rem;
-    font-weight: 500;
-    background-color: ${(props: NavLinkProps) => props.active ? "#f9f4a0" : "transparent"};
+    margin: 2px;
+    font-weight: 700;
+    background-color: transparent;
     border-radius: 10px;
     padding: 5px;
     display: flex;
     align-items: center;
-    justify-content: center;
-    justify-content: space-around;
-
-    a {
-        text-decoration: none;
-        color: #000000;
-    }
-
+    color: ${(props: NavLinkProps) => props.active === "true" ? "#d19a06" : "black"};
+    font-weight: ${(props: NavLinkProps) => props.active === "true" ? "900" : "400"};
+    text-decoration: none;
+    
     svg {
-        margin-right: 0.5rem;
+        margin: 1px;
     }
 
     &:hover {
         background-color: #b9b4b421;
         border-radius: 10px;
-        height: auto;
+    }
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        height: 60px;
+        width: 60px;
+        font-size: small;
+        align-items: center;
+        svg{
+            width: 40px;
+            height: 40px;
+        }
     }
 
 `;
 
-export { Nav, Beta, Logo, NavLinks, NavLink };
+export { Nav, Beta, Logo, MobileLogo, NavLinks, NavLink };
